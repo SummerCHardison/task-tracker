64b9a6acec1dd8fb34bcec4b217d0a9dd3cc0d3e
Summer H <sh.hardison.sh@icloud.com>
2024-06-23 19:58:59 -0400

    this commit contains attempts at getting cards to render and other needed criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,21 @@
 // Retrieve tasks and nextId from localStorage
 const taskList = JSON.parse(localStorage.getItem("tasks"));
 const nextId = JSON.parse(localStorage.getItem("nextId"));
-const formID = document.getElementById('formID')
-const saveButton = document.getElementById('save')
-
-saveButton.addEventListener('click', handleAddTask);
+const formID = document.getElementById('formID');
+const saveButton = document.getElementById('save');
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+  return crypto.randomUUID();
 }
+
+//above function uses crypto to add unique IDs to the tasks
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
   const taskCard = $('<div>')
     .addClass('card project-card draggable my-3')
+    .attr('data-project-id', task.id);
   const cardHeader = $('<div>').addClass('card-header h4').text(localStorage.getItem('taskTitle').value);
   const cardBody = $('<div>').addClass('card-body');
   const cardDescription = $('<p>').addClass('card-text').text(localStorage.getItem('taskDescription').value);
@@ -22,13 +23,23 @@ function createTaskCard(task) {
   const cardDeleteBtn = $('<button>')
     .addClass('btn btn-danger delete')
     .text('Delete')
+    .attr('data-project-id', task.id)
   cardDeleteBtn.on('click', handleDeleteTask);
 
+  cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
+  taskCard.append(cardHeader, cardBody);
+
+  return taskCard;
 }
+//above functions creates a card from bootstrap, however using JS to add the needed elements, classes, attributes, ect (requires debugging)
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
   $(".draggable").draggable();
+  for (const task of tasks) {
+    const taskCard = createTaskCard(task);
+    // $('#task-list').append(taskCard)
+  }
 }
 
 // Todo: create a function to handle adding a new task
@@ -41,7 +52,20 @@ function handleAddTask(event) {
   localStorage.setItem('taskTitle', taskTitle);
   localStorage.setItem('taskDueDate', taskDueDate);
   localStorage.setItem('taskDescription', taskDescription);
+
+
+  const newTask = {
+    title: taskTitle,
+    dueDate: taskDueDate,
+    description: taskDescription
+  }
+
+  const tasks = []
+  tasks.push(newTask)
+
+  localStorage.setItem('tasks', JSON.stringify(tasks))
 }
+//the above function gets the needed elements, adds them to local storage, makes an object with all of the previous information, makes an empty array which has the object pushed into it
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event) {
@@ -55,5 +79,9 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-  $('#datePicker').datepicker()
+  $('#datePicker').datepicker();
+  saveButton.addEventListener('click', handleAddTask);
+  renderTaskList();
   });
+
+  // the above function makes it so that there is a date picker, calls previous functions so they run, and adds any needed event listeners
